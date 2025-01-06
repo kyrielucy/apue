0c30f2d30217662208650aacca914afd64836daa
kyrielucy <kyrielucy@gmail.com>
2025-01-06 11:27:29 +0800

    adjust script

--- subconver.sh
+++ subconver.sh
@@ -1,9 +1,19 @@
  #!/bin/bash
 
+curdir=$(pwd)
+if [ ${XDG_BIN_DIR} ]; then
+    workdir=${XDG_BIN_DIR}
+else
+    workdir=${HOME}/.local/bin/
+fi
+
 pkg="subconverter"
 pkg_tgz="${pkg}_linux64.tar.gz"
-workdir=${HOME}/Public/code/subconver/
 git_url=https://github.com/tindy2013/subconverter/releases/latest/download
+
+# chang to work directory
+[ -d $workdir ] || mkdir -p $workdir
+cd $workdir
 
 [ -f ${pkg_tgz} ] && rm -f ${pkg_tgz}*
 
@@ -13,3 +23,7 @@ if [ -e ${workdir}/${pkg_tgz} ]; then
     tar -xzf ${workdir}/${pkg_tgz} -C ${workdir}
     chmod +rx ${workdir}/${pkg}/${pkg}
 fi
+
+[ -f ${pkg_tgz} ] && rm -f ${pkg_tgz}
+
+cd $curdir
